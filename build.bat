acd5495e66fa51e7a47fe284f17af3809c6b7647
webhook <69124597+SnoopyLeftBehind@users.noreply.github.com>
2023-01-08 11:40:17 +0900

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,23 @@
+@echo off
+
+python --version 2>&1 | findstr " 3.11" >nul
+if %errorlevel% == 0 (
+    echo python 3.11.x and up are not supported by empyrean. Please downgrade to python 3.10.x.
+    pause
+    exit
+)
+
+::fixed by K.Dot cause dif
+git --version 2>&1>nul
+if %errorlevel% == 9009 (
+    echo git is either not installed or not added to path! You can install it here https://git-scm.com/download/win
+    pause
+    exit
+)
+
+cls
+
+if exist build rmdir /s /q build
+py -3.10 builder.py
+
+pause
